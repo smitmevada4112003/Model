f5339e05057086916d4b57928eb1c35d2e3e2e89
smitmevada4112003 <163285461+smitmevada4112003@users.noreply.github.com>
2026-05-07 12:14:47 +0530

    Add files via upload

---
+++ ML PROJECTS/Dockerfile
@@ -0,0 +1,17 @@
+# Use the 'slim' version for a much smaller image size (faster deployments)
+FROM python:3.10-slim
+
+WORKDIR /app
+
+
+COPY requirements.txt .
+
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+COPY . .
+
+EXPOSE 10000
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "10000"]
---
+++ ML PROJECTS/app.py
@@ -0,0 +1,41 @@
+
+#uvicorn app:app --reload
+
+#github - create repo- setting-Secrets and variables-action
+# - new repository secrate - 
+
+# rendor 
+# language - docker 
+
+# after deployment click on setting - deploy hook - copy url
+
+# - https://api.render.com/deploy/srv-d7rkllt7vvec73e8lm4g?key=rB4ttfCiabA
+
+#https://mydep-1a9r.onrender.com/predict
+#https://github.com/joshi510/mydep/settings 
+from fastapi import FastAPI
+import pickle
+
+app = FastAPI()
+
+# load model
+with open("Salary_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.get("/")
+def home():
+    return {"message": "Smit Mevada"}
+
+@app.post("/predict")
+def predict(data: dict):
+    exp = data.get("YearsExperience")
+
+    if exp is None:
+        return {"error": "YearsExperience required"}
+
+    result = model.predict([[exp]])
+
+    return {
+        "YearsExperience": exp,
+        "Salary": int(result[0])
+    }
